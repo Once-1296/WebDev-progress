62ab23cb9a142b7cc36dc8169abe2f5fc43d06b9
Once-1296 <awwab.wadekar@gmail.com>
2025-10-19 16:58:55 +0530

    project base setup

--- Project_Linkly/app.py
+++ Project_Linkly/app.py
@@ -1,55 +1,56 @@
 from flask import Flask, jsonify, request, render_template
+from urllib.parse import urlparse
 import numpy as np
-
+import requests
 app = Flask(__name__)
 
-# 🎯 Generate a random number (1 to 1 billion)
-number = np.random.randint(1, 1000000001)
-guess = None
+map_dict ={
 
+}
+def make_short(link):
+    short_link = "abc"
+    parsed = urlparse(link)
+    if not (parsed.scheme and parsed.netloc):
+        return False, ""
+    try:
+        response = requests.head(link, timeout=5)
+        return True,link
+    except requests.RequestException:
+        return False, ""
+
+    #complete logic
+
+    return True,short_link
 @app.route('/')
 def home():
     # Renders the frontend HTML
     return render_template('index.html')
 
 # POST route: user submits a guess
-@app.route('/api/guess', methods=["POST"])
-def check_guess():
-    global guess
+@app.route('/api/shorten', methods=["POST"])
+def get_link():
+    global map_dict
     data = request.get_json()
-    global number 
     # Validation checks
-    if not data or "num" not in data:
-        return jsonify({"error": "Missing number"}), 400
+    if not data or "link" not in data:
+        return jsonify({"error": "Missing link"}), 400
+    flag,short_link = make_short(data["link"])
+    if not flag:
+        return jsonify({"error": "Invalid link"}), 400
 
-    try:
-        guess = int(data["num"])
-    except ValueError:
-        return jsonify({"error": "Invalid number"}), 400
-
-    print(f"number:{number}")# Compare guess with secret number
-    if guess < number:
-        result = "Too Low!"
-    elif guess > number:
-        result = "Too High!"
-    else:
-        number = np.random.randint(1,1000000001)
-        result = "🎉 Correct! You guessed the number! (Enter new number to try again)"
-
+    map_dict.update({data["link"]:short_link})
      
 
     return jsonify({
-        "your_guess": guess,
-        "result": result
+        "long_link": data["link"],
+        "result": short_link
     }), 200
 
 # Optional GET route (if you want to view the current guess result)
-@app.route('/api/status', methods=["GET"])
+@app.route('/api/show', methods=["GET"])
 def status():
     print("Hello world")
-    if guess is None:
-        return jsonify({"message": "No guess made yet."}), 200
-    return jsonify({"last_guess": guess}), 200
+    return jsonify(map_dict),200
 
 
 if __name__ == '__main__':

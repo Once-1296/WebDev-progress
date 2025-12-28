8b63c5b0b6f1303dbedf798b058624664a3134fe
Once-1296 <awwab.wadekar@gmail.com>
2025-12-29 00:50:38 +0530

    cleaning

---
+++ Day 6/src/App.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./components/HomeSection";
+import AboutPage from "./components/AboutSection";
+import ContactPage from "./components/ContactSection";
+import ListsPage from "./components/ListsSection";
+function App() {
+  return (
+   
+      <Router>
+         <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+        <Routes>
+          
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/lists" element={<ListsPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+         
+        </Routes>
+         </main>
+        <Footer />
+        </div>
+      </Router>
+
+  );
+}
+
+export default App;
---
+++ Day 6/src/components/HomeSection.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HomeSection: React.FC = () => {
+  return (
+    <main>
+    <section className="min-h-screen flex flex-col justify-center items-center text-center  text-white px-4 sm:px-6 md:px-10">
+      <h1 id ="hometext" className="text-5xl  text-blue-600 font-extrabold mb-4">
+        Hi, I’m XYZ. Deligthful greetings(●'◡'●)
+      </h1>
+      <p id ="hometext" className="text-lg text-blue-600 max-w-2xl">
+        some text Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Maiores a fuga minus velit veniam consequuntur tempore quas. Ducimus
+        fugit illo ex maiores dolore voluptas placeat dicta eos sed. Ipsa, a.
+      </p>
+    </section>
+    </main>
+  );
+};
+
+export default HomeSection;
